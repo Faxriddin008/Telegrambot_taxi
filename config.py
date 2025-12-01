07dbcb12e0fc77f71950331e5a12ab419e1b75be
Faxriddin008 <komilovfaxriddin61@gmail.com>
2025-12-01 18:58:43 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# Bot va DB uchun kerakli ma'lumotlar
+BOT_TOKEN = "8512718256:AAGGcXzuf1BLLSYKnhEPbxpTMdlNeWXUQbA"
+
+# MUHIM: Nomi OPERATOR_IDS bo'lishi shart
+OPERATOR_IDS = [
+    5617184769,  # Operator 1 ID'si
+    7394345210   # Operator 2 ID'si
+]
+ # Xabarni yuborish kerak bo'lgan operatorning ID'si
+#5617184769
+# PostgreSQL ulanish ma'lumotlari
+DB_USER = "postgres"
+DB_PASS = "rood"
+DB_PORT = 5432
+DB_HOST = "localhost"
+DB_NAME = "postgres"
